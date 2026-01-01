1a9b3c876c74554f1f445c60379e5a9413a415fa
Shakir ullah <shakir.turk777@gmail.com>
2026-01-01 21:26:06 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-db_url = 'postgresql://postgres:turkish777@localhost:5432/turkish'
+db_url = 'postgresql://postgres:'yourPostgreSQLDataBasePassword'd@localhost:5432/turkish'
 engine = create_engine(db_url)
 session = sessionmaker(autocommit= False , autoflush=False , bind= engine)
 
